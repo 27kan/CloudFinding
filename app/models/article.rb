279224e2abadcd28e0eb5f82f348d459d9b6557e
27kan <79520951+27kan@users.noreply.github.com>
2021-06-07 08:56:52 +0900

    バリデーション設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
+  with_oprions presence: true do
+    validates :title, :content
+    validates :category_id, numericality: { other_than: 0 }
+  end
+  
   belongs_to :user
   has_one_attached :image
 end
