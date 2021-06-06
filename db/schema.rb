46222b21eab0985d5cfe26e534e6058c89974b41
27kan <79520951+27kan@users.noreply.github.com>
2021-06-06 21:01:32 +0900

    データベース反映
    マイグレーションファイルへ反映

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_05_103501) do
+ActiveRecord::Schema.define(version: 2021_06_06_115524) do
+
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content", null: false
+    t.integer "category_id", null: false
+    t.integer "good", null: false
+    t.integer "favorite", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +37,5 @@ ActiveRecord::Schema.define(version: 2021_06_05_103501) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "users"
 end
