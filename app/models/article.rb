ea8d3e924955769e52ade7c145b07baa085a8e1b
27kan <79520951+27kan@users.noreply.github.com>
2021-06-07 21:31:58 +0900

    軽微な修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  with_oprions presence: true do
+  with_options presence: true do
     validates :title, :content
     validates :category_id, numericality: { other_than: 0 }
   end
--- app/models/category.rb
+++ app/models/category.rb
@@ -8,4 +8,5 @@ class Category < ActiveHash::Base
 
   include ActiveHash::Associations
   has_many :articles
+  
 end
