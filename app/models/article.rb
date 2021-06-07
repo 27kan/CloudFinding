39f20dab93eaea9ed17f77f3a3f86a6d9dc58575
27kan <79520951+27kan@users.noreply.github.com>
2021-06-07 09:43:07 +0900

    Categoryの作成
    Categoryリストの作成

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,9 @@ class Article < ApplicationRecord
     validates :title, :content
     validates :category_id, numericality: { other_than: 0 }
   end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
   
   belongs_to :user
   has_one_attached :image
