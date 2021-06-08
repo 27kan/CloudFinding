baf733e37f403e8899de3fed1bb354e253cb6fbe
27kan <79520951+27kan@users.noreply.github.com>
2021-06-08 09:01:07 +0900

    写真がなくても保存できる

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   with_options presence: true do
-    validates :title, :content
+    validates :title
+    validates :content, unless: :was_attached?
     validates :category_id, numericality: { other_than: 0 }
   end
 
@@ -9,4 +10,8 @@ class Article < ApplicationRecord
   
   belongs_to :user
   has_one_attached :image
+
+  def was_attached?
+    self.image.attached?
+  end
 end
