40f64a6fe78fbbf8e28100725a9855a928987745
27kan <79520951+27kan@users.noreply.github.com>
2021-06-10 21:42:34 +0900

    編集機能と削除機能の実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  
   def index
     @articles = Article.all
   end
@@ -20,8 +23,35 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy
+    redirect_to root_path
+  end
+
   private
   def article_params
     params.require(:article).permit(:image, :title, :content, :category_id).merge(user_id: current_user.id)
+  end
+
+  def correct_user
+    # 投稿者以外が編集できないようにする
+    @user = Article.find(params[:id])
+    unless @user.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 end
