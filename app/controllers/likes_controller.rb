88ef3273f20b530829a8988cbc07cf8086d5a5a1
27kan <79520951+27kan@users.noreply.github.com>
2021-07-02 18:08:38 +0900

    commit

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,10 @@ class LikesController < ApplicationController
   def create
     @article = Article.find(params[:article])
     current_user.like(@article)
-    redirect_to root_path
   end
 
   def destroy
     @article = Like.find(params[:id]).article
     current_user.unlike(@article)
-    redirect_to root_path
   end
 end
