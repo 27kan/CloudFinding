6694ee26465a546e6bb07474d8202b8a10e50780
27kan <79520951+27kan@users.noreply.github.com>
2021-07-04 13:26:01 +0900

    非同期再挑戦

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,10 +2,12 @@ class LikesController < ApplicationController
   def create
     @article = Article.find(params[:article])
     current_user.like(@article)
+    redirect_to root_path
   end
 
   def destroy
     @article = Like.find(params[:id]).article
     current_user.unlike(@article)
+    redirect_to root_path
   end
 end
